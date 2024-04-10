99ad371aa10de27de599fb6654c8076d3fa61d69
Dario A. Marin Roth <dariomarinroth@gmail.com>
2024-04-10 09:43:25 -0300

    Completando el Hello Word

--- laboratorios/lab1/led_blink/main.c
+++ laboratorios/lab1/led_blink/main.c
@@ -1,24 +1,43 @@
-
-/* Enciende y apaga un led conectado al puerto B bit 5 de un atmega328 */
-
-/* 
- * El puerto B de un atmega328 tiene los bits 0-5 mapeados a los 
- * pines 8-13 de arduino 
- */
-
 #include "utils.h"
 
-int main(void)
-{	
-	led_init();
+#define LED 0x20                /* 0b00100000: el bit 5 */
+#define CYCLES_PER_MS 450
 
-	while (1) {
-		esperar();
-		led_on();
+void led_init() {
+    volatile unsigned char *DDR_B = (unsigned char *) 0x24;
+    volatile unsigned char *PUERTO_B = (unsigned char *) 0x25;
+    *(PUERTO_B) &= LED;
+    *(DDR_B) |= LED;
+}
 
-		esperar();
-		led_off();
-    	}
+void led_on() {
+    volatile unsigned char *PUERTO_B = (unsigned char *) 0x25;
+    *(PUERTO_B) |= LED;
+}
+
+void led_off() {
+    volatile unsigned char *PUERTO_B = (unsigned char *) 0x25;
+    *(PUERTO_B) &= ~LED;
+}
+
+void esperar() {
+    int milliseconds = 500;
+    volatile unsigned long cycles = (milliseconds * CYCLES_PER_MS);
+
+    while (cycles != 0)
+        cycles--;
+}
+
+int main(void) {
+    led_init();
+
+    while (1) {
+        esperar();
+        led_on();
+
+        esperar();
+        led_off();
+    }
 }
 
 
