99e9eb199f85f8bededbda2a25d6affdf145ffd4
Dario A. Marin Roth <dariomarinroth@gmail.com>
2024-04-30 23:22:27 -0300

    Ultrasónico sin funcionar

--- laboratorios/lab2/ultrasonic/ultrasound.c
+++ laboratorios/lab2/ultrasonic/ultrasound.c
@@ -5,6 +5,7 @@
 #define TRIGGER 1
 #define ECHO 2
 #define TIME_DISTANCE_RELATION 58
+#define TIME_OUT_US 360000
 
 volatile unsigned char *puerto_b = (unsigned char *)0x25;
 volatile unsigned char *ddr_b = (unsigned char *)0x24;
@@ -16,19 +17,24 @@ unsigned int listening_echo();
 
 int main(void) {
   unsigned int echo_time_us;
-  unsigned int distance_cm;
-  unsigned int digit_resolution = 4;
+  float distance_cm = 0;
+  unsigned int integer_digits = 2;
+  unsigned int decimal_digits = 3;
 
   ports_init();
   serial_init();
   serial_put_str("Iniciando \r\n");
+
   while (1) {
     start_ranging();
     echo_time_us = listening_echo();
-    distance_cm = echo_time_us / TIME_DISTANCE_RELATION;
+    distance_cm = ((float)(echo_time_us)) / TIME_DISTANCE_RELATION;
+
     serial_put_str("Distancia en cm: \r\n");
-    serial_put_int(distance_cm, digit_resolution);
+    serial_put_double(distance_cm, integer_digits, decimal_digits);
     serial_put_str("\r\n");
+
+    delay_us(20);
   }
 }
 
@@ -45,11 +51,17 @@ void start_ranging() {
 
 unsigned int listening_echo() {
   unsigned int echo_time_us = 0;
+  serial_put_str("Echo pin en off \r\n");
   while (!is_on(ECHO, *pin_b)) {
   }
+  serial_put_str("Echo pin en on\r\n");
   while (is_on(ECHO, *pin_b)) {
     delay_us(10);
     echo_time_us += 10;
+    if (echo_time_us == TIME_OUT_US) {
+      serial_put_str("FUERA DE RANGO: \r\n");
+      break;
+    }
   }
   return echo_time_us;
 }
--- laboratorios/lab2/ultrasonic/utils.c
+++ laboratorios/lab2/ultrasonic/utils.c
@@ -1,8 +1,8 @@
 
 /* utils.c - funciones de soporte al programa principal */
 
-unsigned char is_on(char bit, unsigned char word) {
-  unsigned char aux = word & (1 << bit);
+unsigned char is_on(unsigned char bit, volatile unsigned char word) {
+    unsigned char aux = word & (1 << bit);
   return aux == (1 << bit);
 }
 
